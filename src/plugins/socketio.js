903968f2c8d5ffb3554877073bcdb456b7f942da
alemagio <alessandro.magionami@gmail.com>
2020-08-20 17:17:37 +0200

    refactor: now using fastify-socket.io plugin

--- src/plugins/socketio.js
+++ src/plugins/socketio.js
@@ -3,5 +3,5 @@
 const fp = require('fastify-plugin')
 
 module.exports = fp(async function(fastify, opts) {
-  fastify.decorate('io', require('socket.io')(fastify.server))
+  fastify.register(require('fastify-socket.io'))
 })
